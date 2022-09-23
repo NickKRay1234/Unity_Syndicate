4dac2f515107b3f95c16b8aef856cc55acaead88
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2022-09-23 23:44:49 +0200

    Added LoadLevelState.cs;

---
+++ Assets/Scripts/Infrastructure/LoadLevelState.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Scripts.Infrastructure
+{
+    public class LoadLevelState : IState
+    {
+        private readonly GameStateMachine _stateMachine;
+        private readonly SceneLoader _sceneLoader;
+
+        public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader)
+        {
+            _stateMachine = stateMachine;
+            _sceneLoader = sceneLoader;
+        }
+
+        public void Enter()
+        {
+            _sceneLoader.Load("Main");
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Exit()
+        {
+        }
+    }
+}
